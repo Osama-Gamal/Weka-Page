a7a8bb2e186a2d10a544093afbd96f4c6a182708
Osama Gamal <97878002+Osama-Gamal@users.noreply.github.com>
2022-09-10 09:07:57 +0200

    main page

---
+++ script/appPage.js
@@ -0,0 +1,26 @@
+
+
+//alert('key1: ' + myData);
+var appUrl = localStorage.getItem("appUrl");
+
+function installApp(){
+    window.open(appUrl, '_blank').focus();
+}
+
+
+function moreApps(){
+    window.location.href="/design/mainPage.html";
+}
+
+function Init(){
+var appName = document.getElementById("appName");
+var appText = document.getElementById("appText");
+var appImage = document.getElementById("appImage");
+var appUrl = document.getElementById("appUrl");
+
+appName.innerHTML = localStorage.getItem("appName");
+appText.innerHTML = localStorage.getItem("appText");
+appImage.src = localStorage.getItem("appImage");
+
+
+}
